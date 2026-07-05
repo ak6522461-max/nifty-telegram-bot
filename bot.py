d14f143f4d1d368d9d48db7641dc4a6c06d63cc2
ak6522461-max <ak6522461@gmail.com>
2026-07-06 00:45:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,11 +9,39 @@ symbol = "^NSEI"
 data = yf.Ticker(symbol)
 hist = data.history(period="2d", interval="5m")
 
-if len(hist) > 0:
-    price = round(hist["Close"].iloc[-1], 2)
-    message = f"📈 NIFTY Live Price: {price}"
+if len(hist) > 20:
+    close = hist["Close"]
+
+    ema9 = close.ewm(span=9).mean()
+    ema21 = close.ewm(span=21).mean()
+
+    price = round(close.iloc[-1], 2)
+
+    if ema9.iloc[-1] > ema21.iloc[-1]:
+        signal = "🟢 BUY"
+        target1 = round(price + 50, 2)
+        target2 = round(price + 100, 2)
+        stoploss = round(price - 50, 2)
+    else:
+        signal = "🔴 SELL"
+        target1 = round(price - 50, 2)
+        target2 = round(price - 100, 2)
+        stoploss = round(price + 50, 2)
+
+    message = f"""
+📊 NIFTY SIGNAL
+
+Price: {price}
+
+Signal: {signal}
+
+🎯 Target 1: {target1}
+🎯 Target 2: {target2}
+🛑 Stop Loss: {stoploss}
+"""
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
     requests.post(url, data={
         "chat_id": CHAT_ID,
         "text": message
